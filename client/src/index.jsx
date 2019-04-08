65639ef609bb058e4c4a46108e19901c51f3fbca
Mark Maher <tenkin@gmail.com>
2019-04-08 16:08:33 -0500

    Added nodemon and npm start script

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 
 class Welcome extends React.Component {
   render() {
-    return <h1>Hello World from React boilerplate</h1>;
+    return <h1>Test</h1>;
   }
 }
 
