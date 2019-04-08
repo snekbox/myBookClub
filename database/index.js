66f4627f4d2398ddc63c2122f8c64530086fd23c
Samuel Keer <sirpizzalot@gmail.com>
2019-04-08 15:53:32 -0500

    added defaults to db connection for client testing

--- database/index.js
+++ database/index.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 require("dotenv").config(); //required dotenv for access to environmental variables
 
 const connection = mysql.createConnection({
-  host: process.env.HOST, //'localhost',
-  user: process.env.MASTER_USER, //'root',
-  port: process.env.PORT,
-  password: process.env.DB_PASSWORD, //''
-  database: process.env.DB_NAME, //'test',
+  host: process.env.HOST || 'localhost',
+  user: process.env.MASTER_USER || 'root',
+  port: process.env.PORT || 3306,
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'test',
 });
 
 const selectAll = (callback) => {
