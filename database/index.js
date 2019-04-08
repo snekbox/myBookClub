a24fbfe4ea4b3fb68b50e2482565c5cca52be138
Samuel Keer <sirpizzalot@gmail.com>
2019-04-08 15:15:56 -0500

    connected to AWS db, added environmental variables and dotenv file

--- database/index.js
+++ database/index.js
@@ -1,10 +1,12 @@
 const mysql = require('mysql');
+require("dotenv").config(); //required dotenv for access to environmental variables
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'test',
+  host: process.env.HOST, //'localhost',
+  user: process.env.MASTER_USER, //'root',
+  port: process.env.PORT,
+  password: process.env.DB_PASSWORD, //''
+  database: process.env.DB_NAME, //'test',
 });
 
 const selectAll = (callback) => {
