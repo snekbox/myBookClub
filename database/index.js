a75d5839e498dd90ad9a64b38e38216210bae516
Quinn McCourt <quinn.mccourt@gmail.com>
2019-04-09 10:45:22 -0500

    Added sequelize connection

--- database/index.js
+++ database/index.js
@@ -1,22 +1,36 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
+const Sequelize = require('sequelize');
 require("dotenv").config(); //required dotenv for access to environmental variables
 
-const connection = mysql.createConnection({
-  host: process.env.HOST || 'localhost',
-  user: process.env.MASTER_USER || 'root',
-  port: process.env.PORT || 3306,
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'test',
-});
-
-const selectAll = (callback) => {
-  connection.query('SELECT * FROM items', (err, items) => {
-    if (err) {
-      callback(err, null);
-    } else {
-      callback(null, items);
-    }
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.MASTER_USER, process.env.DB_PASSWORD, {
+  host: process.env.HOST,
+  port: process.env.PORT,
+  dialect: 'mysql',
+})
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
   });
-};
+// const connection = mysql.createConnection({
+//   host: process.env.HOST || 'localhost',
+//   user: process.env.MASTER_USER || 'root',
+//   port: process.env.PORT || 3306,
+//   password: process.env.DB_PASSWORD || '',
+//   database: process.env.DB_NAME || 'test',
+// });
 
-module.exports.selectAll = selectAll;
+// const selectAll = (callback) => {
+//   connection.query('SELECT * FROM items', (err, items) => {
+//     if (err) {
+//       callback(err, null);
+//     } else {
+//       callback(null, items);
+//     }
+//   });
+// };
+
+// module.exports.selectAll = selectAll;
